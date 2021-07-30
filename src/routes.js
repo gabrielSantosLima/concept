2e300a9f64f57455fbf28d50099af3e2e2a7815f
FMABr <50528285+FMABr@users.noreply.github.com>
2021-07-30 13:04:33 -0400

    API - Get (All) e Delete (Id)

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 const routes = require('express').Router()
 
 const NotesController = require('./controller/NotesController')
+// const NotesService = require('./service/NotesService')
 
 routes.get('/', NotesController.get)
+
+routes.delete('/:noteId(\d+)', NotesController.del)
+
+/* 
+ routes.param('noteId', function(req, res, next, id) {
+    NotesService.get(id)
+})
+*/
 
 module.exports = routes
