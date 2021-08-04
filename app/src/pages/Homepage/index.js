b40fcd848178726e74d72e48d96bad4879c148e8
Raquel Karolyne <karolyneraq@gmail.com>
2021-08-04 12:49:22 -0400

    Landing Page

--- app/src/pages/Homepage/index.js
+++ app/src/pages/Homepage/index.js
@@ -4,7 +4,25 @@ import './styles.css'
 
 const Homepage = () => {
   return(
-      <h1>Página inicial</h1>
+    <div className="content">
+      <aside className="o-aside">
+      <header className="o-title">
+            <h1>Concept</h1>
+      </header>
+      <main className = "o-content">
+        <a>
+          Organize suas ideias<br />
+          em um só lugar.
+        </a>
+      </main>
+      <footer className="o-botao">
+            <button><a>Ir para as minhas notas</a></button>
+      </footer>
+      </aside>
+      <main className="o-main">
+        <img src="Tech Life Communication.svg" alt="" />
+      </main>
+    </div>
   );
 }
 
